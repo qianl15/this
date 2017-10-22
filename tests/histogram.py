87d75d66fae20c9073aba8dd3c823e306ca3ef31
Qian Li <liqian.cs@gmail.com>
2017-10-21 18:02:36 -0700

    updte historgram and util, support 2 videos not

--- tests/histogram.py
+++ tests/histogram.py
@@ -25,12 +25,14 @@ with Database() as db:
   # video path). If any videos fail to ingest, they'll show up in the failed
   # list. If force is true, it will overwrite existing tables of the same
   # name.
-  example_video_path = util.download_video()
+  example_video_path1 = util.download_video(1)
+  example_video_path2 = util.download_video(2)
 
   # test time of ingest
   start = now()
-  [input_table], failed = db.ingest_videos([
-      ('test_raw', example_video_path)], force=True)
+  input_tables, failed = db.ingest_videos([
+      ('test_raw1', example_video_path1), 
+      ('test_raw2', example_video_path2)], force=True)
   print('Time to ingest videos: {:.6f}s'.format(now() - start))
   print(db.summarize())
   print('Failures:', failed)
@@ -53,47 +55,60 @@ with Database() as db:
   # A job defines a table you want to create. In op_args, we bind the frame
   # input column from above to the table we want to read from and name
   # the output table 'example_hist' by binding a string to output_op.
-  job = Job(
-      op_args={
-          frame: db.table('test_raw').column('frame'),
-          output_op: 'test_hist'
-      }
+  job1 = Job(
+    op_args={
+      frame: db.table('test_raw1').column('frame'),
+      output_op: 'test_hist1'
+    }
+  )
+
+  job2 = Job(
+    op_args={
+      frame: db.table('test_raw2').column('frame'),
+      output_op: 'test_hist2'
+    }
   )
 
   # Multiple tables can be created using the same execution graph using
   # a bulk job. Here we specify the execution graph (or DAG) by providing
   # the output_op and also specify the jobs we wish to compute.
-  bulk_job = BulkJob(output=output_op, jobs=[job])
+  bulk_job = BulkJob(output=output_op, jobs=[job1, job2])
 
   # This executes the job and produces the output table. You'll see a progress
   # bar while Scanner is computing the outputs.
   start = now()
-  [output_table] = db.run(bulk_job, force=True)
-  print('Totaltime to decode + compute histograms: {:.6f}s'.format(now() - start))
+  output_tables = db.run(bulk_job, force=True)
+  print('Totaltime to decode + compute histograms: {:.6f}s, including profiling time'.format(now() - start))
 
-  # The profiler contains information about how long different parts of your
-  # computation take to run. We use Google Chrome's trace format, which you
-  # can view by going to chrome://tracing in Chrome and clicking "load" in
-  # the top left.
-  output_table.profiler().write_trace('test_hist.trace')
-  # Each row corresponds to a different part of the system, e.g. the thread
-  # loading bytes from disk or the thread running your kernels. If you have
-  # multiple pipelines or multiple nodes, you will see many of these evaluate
-  # threads.
+  num = 1
+  for output_table in output_tables:
+    # The profiler contains information about how long different parts of your
+    # computation take to run. We use Google Chrome's trace format, which you
+    # can view by going to chrome://tracing in Chrome and clicking "load" in
+    # the top left.
+    # Since scanner will run bulk jobs in one pipeline, the trace file for
+    # jobs are combined into one giant file for the whole bulk jobs
+    if num == 1:
+     output_table.profiler().write_trace('test_hist.trace'.format(num))
+    # Each row corresponds to a different part of the system, e.g. the thread
+    # loading bytes from disk or the thread running your kernels. If you have
+    # multiple pipelines or multiple nodes, you will see many of these evaluate
+    # threads.
 
 
-  # Load the histograms from a column of the output table. The
-  # parsers.histograms  function  converts the raw bytes output by Scanner
-  # into a numpy array for each channel.
-  video_hists = output_table.load(['histogram'], parsers.histograms)
+    # Load the histograms from a column of the output table. The
+    # parsers.histograms  function  converts the raw bytes output by Scanner
+    # into a numpy array for each channel.
+    video_hists = output_table.load(['histogram'], parsers.histograms)
 
-  # Loop over the column's rows. Each row is a tuple of the frame number and
-  # value for that row.
-  num_rows = 0
-  for (frame_index, frame_hists) in video_hists:
-      assert len(frame_hists) == 3
-      assert frame_hists[0].shape[0] == 16
-      num_rows += 1
-  assert num_rows == db.table('test_raw').num_rows()
+    # Loop over the column's rows. Each row is a tuple of the frame number and
+    # value for that row.
+    num_rows = 0
+    for (frame_index, frame_hists) in video_hists:
+        assert len(frame_hists) == 3
+        assert frame_hists[0].shape[0] == 16
+        num_rows += 1
+    assert num_rows == db.table('test_raw{:d}'.format(num)).num_rows()
+    num += 1
 
   print(db.summarize())
--- tests/util.py
+++ tests/util.py
@@ -10,20 +10,33 @@
 import os.path
 
 try:
-    import youtube_dl
+  import youtube_dl
 except ImportError:
-    print('You need to install youtube-dl to run this. Try running:\npip install youtube-dl')
+  print('You need to install youtube-dl to run this. Try running:\npip install youtube-dl')
+  exit()
+
+# VID_PATH = "/tmp/example2.mp4"
+
+def download_video(num = 1):
+  VID_PATH = ""
+  if num == 1:
+    VID_PATH = "/tmp/example.mp4"
+  elif num == 2:
+    VID_PATH = "/tmp/example2.mp4"
+  else:
+    print('invalid option for video: choose 1 or 2')
     exit()
 
-VID_PATH = "/tmp/example2.mp4"
-
-def download_video():
-    if not os.path.isfile(VID_PATH):
-        ydl_opts = {
-            'format': '134',
-            'outtmpl': u'/tmp/example2.%(ext)s'
-        }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            # ydl.download(["https://www.youtube.com/watch?v=79DijItQXMM"])
-            ydl.download(["https://www.youtube.com/watch?v=cPAbx5kgCJo"])
-    return VID_PATH
+  if not os.path.isfile(VID_PATH):
+    ydl_opts = {
+      'format': '134',
+      'outtmpl': u'/tmp/example.%(ext)s'
+    }
+    if num == 2:
+      ydl_opts['outtmpl'] = u'/tmp/example2.%(ext)s'
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+      if num == 1:
+        ydl.download(["https://www.youtube.com/watch?v=79DijItQXMM"])
+      elif num == 2:
+        ydl.download(["https://www.youtube.com/watch?v=cPAbx5kgCJo"])
+  return VID_PATH
