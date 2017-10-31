fbc140ceccd3df1fde6e5d5cd0bab13df0535009
liqian <liqian.cs@gmail.com>
2017-10-30 17:49:40 -0700

    provide bin name

--- tests/lambda_executable/src/lambda_function.py
+++ tests/lambda_executable/src/lambda_function.py
@@ -15,20 +15,24 @@ from executable import Executable
 
 def lambda_handler(event, context):
   user_str = 'aaa'
+  exe_file = 'hello'
   try:
     # API Gateway GET method
     if event['httpMethod'] == 'GET':
       user_str = event['queryStringParameters']['str']
+      exe_file = event['queryStringParameters']['bin']
     # API Gateway POST method
     elif event['httpMethod'] == 'POST':
       data = json.loads(event['body'])
       user_str = data['str']
+      exe_file = data['bin']
   except KeyError:
     # direct invocation
     user_str = event['str']
+    exe_file = event['bin']
 
   user_str = '\"' + user_str + '\"'
-  exe = Executable('executables/hello')
+  exe = Executable('executables/' + exe_file)
   result = exe.run('{}'.format(user_str))
   print('OUT: {}\nERR: {}\nRET: {}'.format(
     exe.stdout, exe.stderr, exe.returncode))
