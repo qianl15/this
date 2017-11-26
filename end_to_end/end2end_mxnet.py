186a99ef8fc0b9a627e1a73904c6475e430e18b8
Qian Li <liqian.cs@gmail.com>
2017-11-25 23:56:05 -0800

    work with new scanner

--- end_to_end/end2end_mxnet.py
+++ end_to_end/end2end_mxnet.py
@@ -17,18 +17,17 @@ from timeit import default_timer as now
 import math
 from urllib import urlretrieve
 
+WORK_PACKET_SIZE = 200  # how many frames to decode together
+BATCH_SIZE = 50
+
 # choose which video we wanted to download, and the format
 # format 134 = 360p, 135 = 480p, 136 = 720p, 137 = 1080p
 # By default, we download the third video with the lowest quality
-def start_mxnet_pipeline(num = 3, fm_num = 1, out_dir = './', batch = 1):
-  if num > 4:
-    test_video_path = util.download_video2('http://web.stanford.edu/~jamesh93/video/wild480p.mkv')
-  else:
-    test_video_path = util.download_video1(num, fm_num)
-
-  print('Batch {:d}, #{:d} video, #{:d} format, outdir: {}'.format(batch, 
-    num, fm_num, out_dir))
-
+# batch - number of frames to do in a MXNet Lambda
+def start_mxnet_pipeline(test_video_path='videos/example.mp4', 
+                         out_dir = './', batch = BATCH_SIZE,
+                         load_to_disk = False):
+  
   if util.have_gpu():
     device = DeviceType.GPU
     print('with GPU device!')
@@ -38,7 +37,7 @@ def start_mxnet_pipeline(num = 3, fm_num = 1, out_dir = './', batch = 1):
 
   script_dir = os.path.dirname(os.path.abspath(__file__))
 
-  # Start Scanner DB
+  # Start Scanner DB, use its load worker to generate .proto and .bin files
   with Database() as db:
     # register the fake kernel
     db.register_op('Fake', [('frame', ColumnType.Video)], ['class'])
@@ -71,37 +70,44 @@ def start_mxnet_pipeline(num = 3, fm_num = 1, out_dir = './', batch = 1):
       }
     )
     bulk_job = BulkJob(output=output_op, jobs=[job])
-    [output_table] = db.run(bulk_job, force=True, profiling=True, pipeline_instances_per_node=1, load_to_disk=True)
+    [output_table] = db.run(bulk_job, force=True, profiling=True, pipeline_instances_per_node=1, load_to_disk=load_to_disk, 
+      work_packet_size=WORK_PACKET_SIZE)
 
     stop = now()
     delta = stop - start
     print('Batch: {:d} End-to-end Fake Python Kernel time: {:.4f}s, {:.1f} fps\n'.format(batch, delta, input_table.num_rows() / delta))
 
     output_table.profiler().write_trace(
-      out_dir + 'end2end_{:d}_{:d}.trace'.format(num, fm_num))
+      out_dir + 'end2end_{:d}.trace'.format(batch))
 
-    video_classes = output_table.load(['class'], parsers.classes)
+    if load_to_disk == False:
+      video_classes = output_table.load(['class'], parsers.classes)
 
-    # Loop over the column's rows. Each row is a tuple of the frame number and
-    # value for that row.
-    num_rows = 0
-    for (frame_index, frame_classes) in video_classes:
-      assert len(frame_classes) == 1
-      assert frame_classes[0].shape[0] == 1
-      # print(frame_classes[0])
-      num_rows += 1
-    assert num_rows == db.table('end2end_raw').num_rows()
+      # Loop over the column's rows. Each row is a tuple of the frame number and
+      # value for that row.
+      num_rows = 0
+      for (frame_index, frame_classes) in video_classes:
+        assert len(frame_classes) == 1
+        assert frame_classes[0].shape[0] == 1
+        # print(frame_classes[0])
+        num_rows += 1
+      assert num_rows == db.table('end2end_raw').num_rows()
 
-    print(db.summarize())
+      print(db.summarize())
+
+  # Then start the Lambda part
+  if load_to_disk == True:
+    pass
 
 if __name__ == '__main__':
   num = 1 # which video
   fm_num = 1 # which resolution
   out_dir = './' # which output directory
-  batch = 1
+  batch = BATCH_SIZE
+  load_to_disk = False;
 
-  if (len(sys.argv) < 1) or (len(sys.argv) > 5):
-    print('Usage: end2end_mxnet.py <video_num> <video_resolution> <out_dir> <batch_size>');
+  if (len(sys.argv) < 1) or (len(sys.argv) > 6):
+    print('Usage: end2end_mxnet.py <video_num> <video_resolution> <out_dir> <batch_size> <load to disk: 0/1>');
     exit()
 
   if (len(sys.argv) > 1):
@@ -112,5 +118,22 @@ if __name__ == '__main__':
     out_dir = sys.argv[3]
   if (len(sys.argv) > 4):
     batch = int(sys.argv[4])
+  if (len(sys.argv) > 5):
+    tmp = int(sys.argv[5])
+    if tmp == 1:
+      load_to_disk = True
+    elif tmp == 0:
+      load_to_disk = False
+    else:
+      print('Please select load_to_disk by 0: False or 1: True')
+      exit()
 
-  start_mxnet_pipeline(num, fm_num, out_dir, batch)
+  if num > 4:
+    test_video_path = util.download_video2('http://web.stanford.edu/~jamesh93/video/wild480p.mkv')
+  else:
+    test_video_path = util.download_video1(num, fm_num)
+
+  print('Batch {:d}, #{:d} video, #{:d} format, outdir: {}'.format(batch, 
+    num, fm_num, out_dir))
+
+  start_mxnet_pipeline(test_video_path, out_dir, batch, load_to_disk)
