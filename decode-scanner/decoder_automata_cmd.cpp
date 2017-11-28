96f274a4d64fcd7171013b9b3a1e32253868242e
Qian Li <liqian.cs@gmail.com>
2017-11-28 01:11:54 -0800

    update decoder output

--- decode-scanner/decoder_automata_cmd.cpp
+++ decode-scanner/decoder_automata_cmd.cpp
@@ -44,58 +44,29 @@ namespace internal {
     checkOutputDir(output_dir);
 
     std::fstream decodeArgsFile(argv[1], std::ios::in | std::ios::binary);
-    //std::fstream encodedFile(argv[2], std::ios::in | std::ios::binary);
+
     proto::DecodeArgs loadedDecodeArgs;
     if (!loadedDecodeArgs.ParseFromIstream(&decodeArgsFile)) {
       std::cerr << "Failed to parse address book." << std::endl;
       return;
     }
-    std::cout << "The loaded decode args are: \n " << loadedDecodeArgs.DebugString() << std::endl;
+    // std::cout << "The loaded decode args are: \n " << loadedDecodeArgs.DebugString() << std::endl;
     std::vector<proto::DecodeArgs> args;
-    /*
-    u8* encodedVideoBuffer = new u8[loadedDecodeArgs.encoded_video_size()];
-    encodedFile.read(encodedVideoBuffer, loadedDecodeArgs.encoded_video_size());
-    */
     
     MemoryPoolConfig config;
     init_memory_allocators(config, {});
 
-    // std::unique_ptr<storehouse::StorageConfig> sc(storehouse::StorageConfig::make_posix_config());
-
-    // auto storage = storehouse::StorageBackend::make_from_config(sc.get());
     VideoDecoderType decoder_type = VideoDecoderType::SOFTWARE;
     DeviceHandle device = CPU_DEVICE;
     DecoderAutomata* decoder = new DecoderAutomata(device, 1, decoder_type);
 
     // Load test data
-    //VideoMetadata video_meta =
-    //  read_video_metadata(storage, download_video_meta(short_video));
     std::string videoFileName = argv[2];
     std::vector<u8> video_bytes = read_entire_file(videoFileName);
     u8* video_buffer = new_buffer(CPU_DEVICE, video_bytes.size());
     memcpy_buffer(video_buffer, CPU_DEVICE, video_bytes.data(), CPU_DEVICE,
       video_bytes.size());
 
-    /*
-    std::vector<proto::DecodeArgs> args;
-    args.emplace_back();
-    proto::DecodeArgs& decode_args = args.back();
-    decode_args.set_width(video_meta.width());
-    decode_args.set_height(video_meta.height());
-    decode_args.set_start_keyframe(0);
-    decode_args.set_end_keyframe(video_meta.frames());
-    for (i64 r = 0; r < video_meta.frames(); ++r) {
-      decode_args.add_valid_frames(r);
-    }
-    for (i64 k : video_meta.keyframe_positions()) {
-      decode_args.add_keyframes(k);
-    }
-    for (i64 k : video_meta.keyframe_byte_offsets()) {
-      decode_args.add_keyframe_byte_offsets(k);
-    }
-    
-    decode_args.set_encoded_video_size(video_bytes.size());
-    */
     loadedDecodeArgs.set_encoded_video((i64)video_buffer);
 
     args.push_back(loadedDecodeArgs);
@@ -109,14 +80,11 @@ namespace internal {
     std::vector<int> encode_params;
     encode_params.push_back(CV_IMWRITE_JPEG_QUALITY);
     encode_params.push_back(100);
+    size_t total_size = 0;
     for (i64 i = 0; i < loadedDecodeArgs.valid_frames().size(); ++i) {
       decoder->get_frames(frame_buffer.data(), 1);
       int ind = loadedDecodeArgs.valid_frames()[i];
       std::string ind_str = std::to_string(ind);
-      
-      // printf("for frame %li the red component of a pixel has value: %i \n", i, frame_buffer[300000]);
-      // printf("for frame %li the green component of the a pixel has value: %i \n", i, frame_buffer[300001]);
-      // printf("for frame %li the blue component of the a pixel has value: %i \n", i, frame_buffer[300002]);
 
       const scanner::Frame* frame = new Frame(frame_info, frame_buffer.data());
       cv::Mat img = scanner::frame_to_mat(frame);
@@ -132,11 +100,19 @@ namespace internal {
       std::ofstream output_buff(output_dir + "/frame" + ind_str + ".jpg",
           std::ios::out | std::ios::trunc | std::ios::binary);
       output_buff.write(str_encode.c_str(), str_encode.size());
-      printf("Save frame%d.jpg to disk \n", ind);
+      if (output_buff.fail()) {
+        printf("Failed to save frame%d.jpg to disk, size %lu KB\n",
+          ind, str_encode.size() / 1024);
+        exit(-1);
+      }
+      printf("Save frame%d.jpg to disk, size %lu KB\n", 
+              ind, str_encode.size() / 1024);
+      total_size += str_encode.size();
       delete frame;
     }
 
-    
+    printf("# %lu Files. Total size is: %lu MB\n", 
+           loadedDecodeArgs.valid_frames().size(), total_size / (1024 * 1024));
     delete decoder;
     // delete storage;
     destroy_memory_allocators();
